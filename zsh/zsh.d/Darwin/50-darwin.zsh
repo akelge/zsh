0a42992bb872e00d12a5e3f7136de503c046e8a5
Akelge <akelge@gmail.com>
2013-07-09 19:57:45 +0000

    Remove mvim alias Added diff file to make mvim work

--- zsh/zsh.d/Darwin/50-darwin.zsh
+++ zsh/zsh.d/Darwin/50-darwin.zsh
@@ -8,6 +8,6 @@ alias lsrebuild='/System/Library/Frameworks/CoreServices.framework/Versions/A/Fr
 
 alias vim='/Applications/MacVim.app/Contents/MacOS/Vim'
 # alias mvim='mvim --remote-tab-silent '
-alias mvim='open -a MacVim '
+# alias mvim='open -a MacVim '
 
 #  vim: set ts=4 sw=4 tw=0 ft=zsh : 
