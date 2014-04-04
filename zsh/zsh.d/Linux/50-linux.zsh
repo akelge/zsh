e72a96f37de4cddc119970c283040481105e2854
Akelge <AkelGE@gmail.com>
2014-04-04 18:05:30 +0000

    Changed ls alias

--- zsh/zsh.d/Linux/50-linux.zsh
+++ zsh/zsh.d/Linux/50-linux.zsh
@@ -5,7 +5,7 @@ else
     eval `dircolors`
 fi
 
-alias ls='ls -F --color=auto'
+alias ls='LANG=C ls -F --color=auto --group-directories-first'
 alias open='xdg-open '
 
 #  vim: set ts=4 sw=4 tw=0 ft=zsh : 
