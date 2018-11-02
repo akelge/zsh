901d42d822147030c2420766d758a96626585434
Andrea Mistrali <andmis@jpj.com>
2018-11-02 15:32:54 +0100

    Changed venv

--- zsh.d/90-virtualenv.zsh
+++ zsh.d/90-virtualenv.zsh
@@ -8,48 +8,22 @@ function venv {
         return
     fi
 
-    VIRTUALENV_SCRIPT=/usr/local/bin/virtualenvwrapper.sh
+    [ -z $VIRTUALENV_SCRIPT ] && VIRTUALENV_SCRIPT=/usr/local/bin/virtualenvwrapper.sh
 
     [ -d $WORKON_HOME ] || mkdir -p $WORKON_HOME
     [ -d $PROJECT_HOME ] || mkdir -p $PROJECT_HOME
 
     source $VIRTUALENV_SCRIPT
 
-    echo "virtualenv enabled."
-    echo "WORKON_HOME: $WORKON_HOME"
-    echo "PROJECT_HOME: $PROJECT_HOME"
-    echo
-    echo "Virtualenvs:"
-    lsvirtualenv -b
-    echo
-}
-
-function venv3 {
-
-    # export WORKON_HOME=$HOME/Src/virtualenvs
-    # export PROJECT_HOME=$HOME/Src/projects
-    if [ -z $WORKON_HOME ] || [ -z $PROJECT_HOME ]; then
-        echo "Please, set WORKON_HOME and PROJECT_HOME first."
-        echo "Better place to set them is 99-local.zsh"
-        return
+    if [ -z $1 ]; then
+        echo "virtualenv enabled."
+        echo "WORKON_HOME: $WORKON_HOME"
+        echo "PROJECT_HOME: $PROJECT_HOME"
+        echo
+        echo "Virtualenvs:"
+        lsvirtualenv -b
+        echo
+    else
+        workon $1
     fi
-
-    VIRTUALENV_SCRIPT=/usr/local/bin/virtualenvwrapper.sh
-    export VIRTUALENVWRAPPER_PYTHON=/usr/local/bin/python3
-    export VIRTUALENVWRAPPER_VIRTUALENV=/usr/local/bin/virtualenv
-    # export VIRTUALENVWRAPPER_VIRTUALENV_ARGS='--no-site-packages'
-
-    [ -d $WORKON_HOME ] || mkdir -p $WORKON_HOME
-    [ -d $PROJECT_HOME ] || mkdir -p $PROJECT_HOME
-
-    source $VIRTUALENV_SCRIPT
-
-    echo "virtualenv enabled."
-    echo "WORKON_HOME: $WORKON_HOME"
-    echo "PROJECT_HOME: $PROJECT_HOME"
-    echo
-    echo "Virtualenvs:"
-    lsvirtualenv -b
-    echo
 }
-# vim: set ts=4 sw=4 tw=0 ft=sh : 
