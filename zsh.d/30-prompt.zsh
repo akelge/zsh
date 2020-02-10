ce1b4c9f5ec3461a57b7058ee3ec2b94520fd53e
Andrea <andre@jenny.ifson.lan>
2020-02-10 16:23:56 +0100

    Added pure prompt

--- zsh.d/30-prompt.zsh
+++ zsh.d/30-prompt.zsh
@@ -1,5 +1,11 @@
 fpath=($0:h/prompts $fpath)
+fpath+=$0:h/prompts/pure
 
 autoload -U promptinit;promptinit
+
+zstyle :prompt:pure:user color blue
+zstyle :prompt:pure:host color blue
+
+prompt pure
 
 #  vim: set ts=4 sw=4 tw=0 ft=zsh : 
