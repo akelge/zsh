d9f26224c3ed78e697fed8ce738ef91b1cf6c00a
Andrea Mistrali <akelge@gmail.com>
2020-02-13 16:06:21 +0100

    Comented out useless vim aliases

--- zsh.d/Darwin/50-darwin.zsh
+++ zsh.d/Darwin/50-darwin.zsh
@@ -6,8 +6,8 @@ alias ls='ls -F -G'
 alias skill=killall
 alias lsrebuild='/System/Library/Frameworks/CoreServices.framework/Versions/A/Frameworks/LaunchServices.framework/Versions/A/Support/lsregister -kill -r -domain local -domain system -domain user'
 
-alias vim='/Applications/MacVim.app/Contents/MacOS/Vim '
-alias vi='/Applications/MacVim.app/Contents/MacOS/Vim '
+# alias vim='/Applications/MacVim.app/Contents/MacOS/Vim '
+# alias vi='/Applications/MacVim.app/Contents/MacOS/Vim '
 # alias mvim='mvim --remote-tab-silent '
 # alias mvim='open -a MacVim '
 
