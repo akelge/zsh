58f5f253252ae792f63c4e5ea313c013c93e04fc
root <root@LV6KVY0Z2.veriskdom.com>
2020-04-17 11:15:12 +0200

    Added awslogin function, that uses saml2aws

--- zsh.d/50-functions.zsh
+++ zsh.d/50-functions.zsh
@@ -19,11 +19,11 @@ autoload -U bak
 # fancyTerm - returns true if we are on xterm/rxvt/screen, elsewhere false
 autoload -U fancyTerm
 
-# repo_char - returns a character based on the type of repo we are in
-autoload -U repo_char
+# # repo_char - returns a character based on the type of repo we are in
+# autoload -U repo_char
 
-# repo_char - returns a character based on the type of repo we are in
 autoload -U x509
 
+autoload -U awslogin
 
 #  vim: set ts=4 sw=4 tw=0 ft=zsh : 
