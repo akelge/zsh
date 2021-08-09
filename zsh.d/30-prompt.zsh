b47852808f348dfa3a64c0a65d55c54c1c8a40a7
Andrea Mistrali <andrea.mistrali@technest.es>
2021-08-09 16:30:39 +0200

    longer prompt time

--- zsh.d/30-prompt.zsh
+++ zsh.d/30-prompt.zsh
@@ -7,8 +7,11 @@ autoload -U promptinit;promptinit
 zstyle :prompt:pure:user color white
 zstyle :prompt:pure:host color white
 
+PURE_CMD_MAX_EXEC_TIME=30
+
 # Default prompt is pure
 #
-prompt mypure
+# prompt mypure
+prompt pure
 
 #  vim: set ts=4 sw=4 tw=0 ft=zsh : 
