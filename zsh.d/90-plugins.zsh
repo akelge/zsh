a65005aa7deacacd3f6c2174aa790372cf4779e7
Andrea Mistrali <andrea.mistrali@gmail.com>
2021-10-28 10:39:10 +0200

    Plugin messages

--- zsh.d/90-plugins.zsh
+++ zsh.d/90-plugins.zsh
@@ -3,10 +3,12 @@ if whence pyenv > /dev/null; then
   export PYENV_ROOT="$HOME/.pyenv"
   eval "$(pyenv init --path)"
   eval "$(pyenv init -)"
+else
+  echo "pyenv not installed"
 fi
 
 # thefuck
-whence  thefuck > /dev/null && eval $(thefuck --alias)
+whence  thefuck > /dev/null && eval $(thefuck --alias) || echo "thef**k not installed"
 
 #z
 [ -f $LIBRARY/plugins/zsh-z/zsh-z.plugin.zsh ] && source $LIBRARY/plugins/zsh-z/zsh-z.plugin.zsh
