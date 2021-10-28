7023dd4906c81592abbada84741e12ad8392b42b
Andrea Mistrali <andrea.mistrali@gmail.com>
2021-10-28 10:40:01 +0200

    Revert message on missing plugins

--- zsh.d/90-plugins.zsh
+++ zsh.d/90-plugins.zsh
@@ -3,12 +3,10 @@ if whence pyenv > /dev/null; then
   export PYENV_ROOT="$HOME/.pyenv"
   eval "$(pyenv init --path)"
   eval "$(pyenv init -)"
-else
-  echo "pyenv not installed"
 fi
 
 # thefuck
-whence  thefuck > /dev/null && eval $(thefuck --alias) || echo "thef**k not installed"
+whence  thefuck > /dev/null && eval $(thefuck --alias)
 
 #z
 [ -f $LIBRARY/plugins/zsh-z/zsh-z.plugin.zsh ] && source $LIBRARY/plugins/zsh-z/zsh-z.plugin.zsh
