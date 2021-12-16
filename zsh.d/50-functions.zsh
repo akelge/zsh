ca8a24d28ed269881d879f7892b70127aa0b8893
Andrea Mistrali <akelge@gmail.com>
2021-12-16 08:54:17 +0100

    remove zwc files, add gitfetch function
    Also load modules for stat and time;
    Fix status typos

--- zsh.d/50-functions.zsh
+++ zsh.d/50-functions.zsh
@@ -1,6 +1,14 @@
 #######################################################################################
 # Functions
 #
+#
+#
+#
+# Load usefule modules
+
+zmodload -F zsh/stat b:zstat
+zmodload zsh/datetime
+#
 # Autoload all functions in zshfunctions
 #
 autoload -U $LIBRARY/zshfunctions/*(.:t)
