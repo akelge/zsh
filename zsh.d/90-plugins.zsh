1267b3033ff0e76ac130d4c45088e4aebeceb36d
Andrea Mistrali <akelge@gmail.com>
2023-10-10 13:13:49 +0200

    Typo

--- zsh.d/90-plugins.zsh
+++ zsh.d/90-plugins.zsh
@@ -11,7 +11,7 @@ whence  thefuck > /dev/null && eval $(thefuck --alias)
 # z
 # [ -f $LIBRARY/plugins/zsh-z/zsh-z.plugin.zsh ] && source $LIBRARY/plugins/zsh-z/zsh-z.plugin.zsh
 if whence zoxide > /dev/null; then  ## zoxide is installed
-  eval $(zoxide init zsh)
+  eval "$(zoxide init zsh)"
   export Z_COMMAND=zoxide
 elif [ -f $LIBRARY/plugins/zsh-z/zsh-z.plugin.zsh ]; then ## zsh-z is installed
   source $LIBRARY/plugins/zsh-z/zsh-z.plugin.zsh
