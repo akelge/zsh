c58c9d82c5edf1972946591cb9385a025efead0b
Andrea <akelge@gmail.com>
2026-05-15 11:01:46 +0200

    Speed up compinit startup

--- zsh.d/20-complete.zsh
+++ zsh.d/20-complete.zsh
@@ -1,16 +1,22 @@
-#######################################################################################
-# Start autocomplete
-autoload -Uz compinit bashcompinit
+local ZCOMPDUMP="$HOME/.zcompdump"
 
-for dump in ~/.zcompdump(N.mh+24); do
-    compinit
-    touch ~/.zcompdump
-done
+# Check if the dump is old or missing
+if [[ ! -f "$ZCOMPDUMP" || ! "${ZCOMPDUMP}"(N.mh-24) ]]; then
+  # This block runs only once every 24 hours
+  autoload -Uz compinit
+  # Run a full compinit with the security audit enabled
+  compinit -d "$ZCOMPDUMP"
+  touch "$ZCOMPDUMP"
+else
+  # This block runs for every other shell start (Fast Path)
+  # We use -C to skip compaudit, but it's safe because we verified
+  # the files 24 hours ago.
+  autoload -Uz compinit
+  compinit -C -d "$ZCOMPDUMP"
+fi
 
-compinit -C
-
+autoload -Uz bashcompinit
 bashcompinit
-# autoload bashcompinit
 
 # allow approximate
 zstyle ':completion:*' completer _complete _match _approximate
